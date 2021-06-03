38713b604e1040c8dc4fde9946f1225f6b855b57
Bear <99shiya@gmail.com>
2021-06-03 12:36:50 +0800

    '新增會員登入功能'

---
+++ pracGit/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pracGit.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string account,string password)
+        {
+            return View("Login");
+        }
+    }
+}
